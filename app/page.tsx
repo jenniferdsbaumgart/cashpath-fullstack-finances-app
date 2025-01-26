5065a180637738a3064863b26a0158026bb8ed0c
Jennifer <Jennifer.baumgart@minervacontrols.com>
2025-01-26 16:17:07 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return <h1 className="text-red-500">home</h1>;
+  return <Button>Hello world</Button>;
 };
 
 export default Home;
