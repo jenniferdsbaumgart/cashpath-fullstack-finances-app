324391da066803b9fafd4a3b23e54e60aa9b155b
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-05-03 16:31:20 -0300

    feat: add ai report generation

---
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -0,0 +1,51 @@
+"use server";
+import { db } from "@/app/_lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import OpenAI from "openai";
+import { GenerateAiReportSchema, generateAiReportSchema } from "./schema";
+
+export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
+  generateAiReportSchema.parse({ month });
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
+  if (!hasPremiumPlan) {
+    throw new Error("You need a premium plan to use this feature");
+  }
+
+  const openAi = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  // get the monthly transactions for the user
+  const transactions = await db.transaction.findMany({
+    where: {
+      date: {
+        gte: new Date(`2025-${month}-01`),
+        lte: new Date(`2025-${month}-31`),
+      },
+    },
+  });
+  // send to chatgpt and as to generate the report with insights
+  const content = `Generate a report of my following finance transactions with insights and suggestions for improve my finance management. The transactions are divided by dot and comma. The structure of each transaction is {DATE}--{TYPE}--{VALUE}--{CATEGORY}. The transactions are: ${transactions.map((transaction) => `${transaction.date.toLocaleDateString("pt-BR")}--${transaction.type}--US$${transaction.amount}--${transaction.category}`).join(";")}`;
+  const response = await openAi.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a financial advisor. You are very good at generating reports and giving insights about finance management.",
+      },
+      {
+        role: "user",
+        content,
+      },
+    ],
+    max_tokens: 1000,
+    temperature: 0.7,
+  });
+  // return the report
+  return response.choices[0].message.content;
+};
---
+++ app/(home)/_components/ai-report-button.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { DialogFooter, DialogHeader } from "@/app/_components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import React, { useState } from "react";
+import { generateAiReport } from "../_actions/generate-ai-report";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import Markdown from "react-markdown";
+
+interface AiReportButtonProps {
+  month: string;
+}
+
+const AiReportButton = ({ month }: AiReportButtonProps) => {
+  const [report, setReport] = useState<string | null>(null);
+  const [reportIsLoading, setReportIsLoading] = useState(false);
+
+  const handleGenerateReportClick = async () => {
+    try {
+      setReportIsLoading(true);
+      const aiReport = await generateAiReport({ month });
+      setReport(aiReport);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setReportIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          AI Report
+          <BotIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>AI Report</DialogTitle>
+          <DialogDescription>
+            Use AI to generate a report of your transactions with insights.
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+          <Markdown>{report}</Markdown>
+        </ScrollArea>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="ghost">Cancel</Button>
+          </DialogClose>
+          <Button
+            onClick={handleGenerateReportClick}
+            disabled={reportIsLoading}
+          >
+            {reportIsLoading && <Loader2Icon className="animate-spin" />}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AiReportButton;
