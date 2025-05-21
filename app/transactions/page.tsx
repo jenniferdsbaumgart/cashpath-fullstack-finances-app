12cba3c91f5e098fb1e40ab10fbe8171e2585fa1
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-05-21 13:54:26 -0300

    feat:  add sharp dependency

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -28,7 +28,7 @@ const TransactionsPage = async () => {
       <Navbar />
       <div className="space-y-6 overflow-hidden p-6">
         {/* TITLE AND BUTTON */}
-        <div className="flex w-full flex-col items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transactions</h1>
           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
